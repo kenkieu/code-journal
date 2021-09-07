074b184b94ca9a8f0d2d01ada565c9e3ff7692c3
Ken Kieu <kenkieu93@gmail.com>
2021-09-07 15:37:55 -0700

    Add listener for input events on photoUrl, and change the src to preview image.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 /* global data */
 /* exported data */
+
+function handlePhoto(event) {
+  var userUrl = event.target.value;
+  $img.setAttribute('src', userUrl);
+}
+
+var $photoUrl = document.querySelector('#photo-url');
+
+var $img = document.querySelector('img');
+$photoUrl.addEventListener('input', handlePhoto);
