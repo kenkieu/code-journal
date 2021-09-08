9d0ccb0ca1b9b6ad19474238fc8ee6a4b7c41281
Ken Kieu <kenkieu93@gmail.com>
2021-09-07 17:20:28 -0700

    Completed Listen for submit events, and all sub-steps.

--- js/main.js
+++ js/main.js
@@ -7,6 +7,30 @@ function handlePhoto(event) {
 }
 
 var $photoUrl = document.querySelector('#photo-url');
-
+// var $title = document.querySelector('#title');
+// var $notes = document.querySelector('#notes');
 var $img = document.querySelector('img');
 $photoUrl.addEventListener('input', handlePhoto);
+
+var $form = document.querySelector('form');
+// console.dir($form);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var entry = {};
+  entry.title = $form.elements[0].value;
+  entry.photoUrl = $form.elements[1].value;
+  entry.note = $form.elements[2].value;
+  entry.entryId = data.nextEntryId;
+  // console.log(entry);
+  data.nextEntryId++;
+  data.entries.unshift(entry);
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.elements[0].value = '';
+  $form.elements[1].value = '';
+  $form.elements[2].value = '';
+}
+
+$form.addEventListener('submit', handleSubmit);
+// console.dir($form.elements);
+// console.log();
